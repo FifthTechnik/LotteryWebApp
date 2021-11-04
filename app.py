d039d7306f6f3cf076fa63476c3fd9f491603a0c
William Stairs <stairswjc@gmail.com>
2021-11-04 23:33:47 +0000

    updates

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class SecurityFilter(logging.Filter):
         return "SECURITY" in record.getMessage()
 
 
-fh = logging.FileHandler('lottery.log', 'w')
+fh = logging.FileHandler('lottery.log', 'a')
 fh.setLevel(logging.WARNING)
 fh.addFilter(SecurityFilter())
 formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
